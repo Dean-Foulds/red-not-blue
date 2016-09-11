22c7d6773abe34ea122ce463876a4eaced5f469f
Dean Foulds <deanfoulds@gmail.com>
2016-09-11 16:39:57 +0100

    params added to post but still have to migrate for post params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,30 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+  def index
+    @post = Post.all
+  end
+  def show
+    @post = Post.new
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:post_content, :title, :pictures)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+Comment.destroy_all
+User.destroy_all
+Post.destroy_all
