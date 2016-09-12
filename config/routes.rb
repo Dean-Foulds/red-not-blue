557cbfc5c35d90a5df01fbc6d7431aea1489e3ac
Dean Foulds <deanfoulds@gmail.com>
2016-09-12 15:22:15 +0100

    comments routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   mount Attachinary::Engine => "/attachinary"
   root to: 'posts#index'
-  resources :posts, only: [:index, :show, :new, :create]
-  # do
-  #   resources :comments, only: [:show, :edit, :update, :create, :destroy]
-  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # end
+  resources :posts, only: [:index, :show, :new, :create] do
+    resources :comments, only: [:show, :edit, :update, :create, :destroy]
+  end
 end
