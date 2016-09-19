d993128520190198546e32773548cbe64270ba07
Dean Foulds <deanfoulds@gmail.com>
2016-09-19 15:08:16 +0100

    meta data added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options =  { host: "red-not-blue.herokuapp.com" }
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
