8e4326e1ce862c4edd50febbc57ba0e33fa90fd9
Dean Foulds <deanfoulds@gmail.com>
2016-09-20 11:10:17 +0100

    removed code from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,10 +13,6 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
-  def default_headers
-    headers['X-Frame-Options'] = 'ALLOWALL'
-  end
-
 
   private
 
