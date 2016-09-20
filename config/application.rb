564a063fbdbab33eacf6cf098e3f23cb8a18a71c
Dean Foulds <deanfoulds@gmail.com>
2016-09-20 11:07:58 +0100

    xframe again

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,6 @@ module RedBlogApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
   end
 end
