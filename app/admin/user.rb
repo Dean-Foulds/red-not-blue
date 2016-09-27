f98c8abde7cebac29b5176fe3846a5aae153832a
Dean Foulds <deanfoulds@gmail.com>
2016-09-27 17:43:08 +0100

    active user model edited to permit two params

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,5 @@
 # ActiveAdmin.register User do
-
+ermit_params :email, :username
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
