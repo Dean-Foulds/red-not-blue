48c3728fb394599008524c25a852fbe836f70f0d
Dean Foulds <deanfoulds@gmail.com>
2016-09-27 17:48:38 +0100

    change user confiq as devises said no

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,4 @@
 # ActiveAdmin.register User do
-permit_params :email, :username
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
